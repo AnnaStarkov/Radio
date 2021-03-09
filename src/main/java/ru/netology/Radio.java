218f997c15e61a6ff2c7e51ef56b348ddab86f44
Anna Starkova <annastarkova2018@gmail.com>
2021-03-09 22:01:27 +0500

    formatting

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -7,28 +7,35 @@ public class Radio {
     public int getCurrentNumber() {
         return this.currentNumber;
     }
-    public void setCurrentNumber(int newNumber) {this.currentNumber = newNumber;}
+
+    public void setCurrentNumber(int newNumber) {
+        this.currentNumber = newNumber;
+    }
 
     public int getCurrentVolume() {
         return this.currentVolume;
     }
+
     public void setCurrentVolume(int newVolume) {
         this.currentVolume = newVolume;
     }
 
-    public void nextStation() { this.currentNumber = (this.currentNumber + 1) % 10; }
+    public void nextStation() {
+        this.currentNumber = (this.currentNumber + 1) % 10;
+    }
+
     public void prevStation() {
         this.currentNumber = (this.currentNumber + 9) % 10;
     }
 
     public void plusVolume() {
-        if (this.currentVolume < 10){
+        if (this.currentVolume < 10) {
             this.currentVolume++;
         }
     }
 
     public void minusVolume() {
-        if (this.currentVolume > 0){
+        if (this.currentVolume > 0) {
             this.currentVolume--;
         }
     }
